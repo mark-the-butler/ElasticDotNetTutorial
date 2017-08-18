1f83f2ee2e886776cdc4741179f512924d9d1c48
mdb513 <mark.d.butler@sherwin.com>
2017-08-18 10:26:10 -0400

    First commit

---
+++ Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ElasticTutorial
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Nest;
+using System;
+
+namespace ElasticTutorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var local = new Uri("http://localhost.docker:9200");
+            var settings = new ConnectionSettings(local);
+            var elastic = new ElasticClient(settings);
+
+            var res = elastic.ClusterHealth();
+
+            Console.WriteLine(res.ClusterName);
+            Console.ReadLine();
+        }
+    }
+}
